6356483670579439412fd46994566d48203caae4
Ferran Closa Cervera <36318284+ferranclosa@users.noreply.github.com>
2021-03-26 18:17:27 +0100

    Initial Commit Added Gateway functionality Added OAuth2 Resource-server functionality Added OAuth2 recognizer functionality

--- src/main/java/com/closa/authentication/AuthenticationApplication.java
+++ src/main/java/com/closa/authentication/AuthenticationApplication.java
@@ -1,13 +1,21 @@
 package com.closa.authentication;
 
+import com.closa.global.context.UserContextInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.client.RestTemplate;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+import java.util.List;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {
@@ -31,6 +39,23 @@ public class AuthenticationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AuthenticationApplication.class, args);
+    }
+
+    @SuppressWarnings("unchecked")
+    @LoadBalanced
+    @Bean
+    public RestTemplate getRestTemplate(){
+        RestTemplate template = new RestTemplate();
+        List interceptors = template.getInterceptors();
+        if (interceptors==null){
+            template.setInterceptors(Collections.singletonList(new UserContextInterceptor()));
+        }
+        else{
+            interceptors.add(new UserContextInterceptor());
+            template.setInterceptors(interceptors);
+        }
+
+        return template;
     }
 
 }
